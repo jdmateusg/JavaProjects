6bb55471066a8d0e6d0fc81db87776727ea9593c
jdmateusg <142945229+jdmateusg@users.noreply.github.com>
2023-09-05 17:22:48 -0500

    Add files via upload

---
+++ Loops/ForExample1.java
@@ -0,0 +1,12 @@
+
+package javaapplication13;
+
+public class ForExample1{
+
+    public static void main(String[] args) {
+        
+     for(;;){
+         System.out.println("infinitive loop");
+     }
+    }
+}
