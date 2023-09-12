487f5d783879e2f30a78f13d82dc06b7516a3ac6
jdmateusg <142945229+jdmateusg@users.noreply.github.com>
2023-09-11 23:22:21 -0500

    Add files via upload

---
+++ PrimerosCodigos/stringExample.java
@@ -0,0 +1,6 @@
+public class stringExample{
+	public static void main(String[] args){	
+	String openingLyrics = "Yesterday, all my troubles seemed so far away";
+	System.out.println(openingLyrics);
+}
+}
