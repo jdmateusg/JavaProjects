c6c171e910557c947a0c22412f98e07cd3c42fb9
jdmateusg <142945229+jdmateusg@users.noreply.github.com>
2023-09-24 18:21:23 -0500

    Add files via upload

---
+++ PrimerosCodigos/compoundOperatorsExample.java
@@ -0,0 +1,9 @@
+public class compoundOperatorsExample{
+	public static void main(String[] args){
+	  int numCookies = 17;
+	  numCookies -=3;
+	  System.out.println(numCookies);
+	  numCookies /=2;
+	  System.out.println(numCookies);
+	}
+}
