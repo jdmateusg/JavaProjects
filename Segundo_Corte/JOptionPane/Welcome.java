d469729472308164d4e5db4d30d9faf449d0868c
jdmateusg <142945229+jdmateusg@users.noreply.github.com>
2023-10-03 17:00:10 -0500

    Add files via upload

---
+++ Segundo_Corte/JOptionPane/Welcome.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Welcome;
+
+import javax.swing.JOptionPane;
+
+public class Welcome{
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "!Welcome\nto\nthe Java world!");
+        System.exit(0);
+    }
+    
+}
