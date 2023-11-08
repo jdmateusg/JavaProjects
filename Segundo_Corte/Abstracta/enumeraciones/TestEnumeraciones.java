74ceee1080a7357c55fc666045639ba2bff8839a
jdmateusg <142945229+jdmateusg@users.noreply.github.com>
2023-11-08 13:33:50 -0500

    Add files via upload

---
+++ Segundo_Corte/Abstracta/enumeraciones/TestEnumeraciones.java
@@ -0,0 +1,21 @@
+package Test;
+
+import enumeraciones.*;
+	
+public class TestEnumeraciones {
+	
+	public static void main(String[] args){
+		
+		System.out.println(Dias.Domingo);
+		System.out.println(Dias.Viernes);
+		
+		System.out.println(Continentes.America);
+		System.out.println(Continentes.America.getPaises());
+		
+		Continentes continente = Continentes.America;
+		System.out.println(continente.getPaises());
+		
+	}
+	
+	
+}
